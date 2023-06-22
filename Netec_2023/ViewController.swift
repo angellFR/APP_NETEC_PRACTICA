652e361162a1546aa6b05e0ae866a9d8b0c82154
Armada PC <angell_rojo@icloud.com>
2023-06-21 22:19:07 -0600

    Initial Commit

---
+++ Netec_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netec_2023
+//
+//  Created by Administrator on 21/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
